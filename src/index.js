014ea6950040304b3ffe0713cad98c8e42f00fe0
bhadresharya <bhadresharya@hotmail.com>
2022-12-25 03:04:38 +0530

    Exploring mantine library

--- src/index.js
+++ src/index.js
@@ -11,9 +11,25 @@ root.render(
       withGlobalStyles
       withNormalizeCSS
       theme={{
-        focusRing : 'always'
-      }}>
+        colorScheme: 'dark',
+        colors: {
+          // override dark colors to change them for all components
+          dark: [
+            '#d5d7e0',
+            '#acaebf',
+            '#8c8fa3',
+            '#666980',
+            '#4d4f66',
+            '#34354a',
+            '#2b2c3d',
+            '#1d1e30',
+            '#0c0d21',
+            '#01010a',
+          ],
+        },
+      }}
+    >
       <App />
     </MantineProvider>
-  </React.StrictMode>
+  </React.StrictMode >
 );
