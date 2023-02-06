e3f3f863f305168dc89bd2bb2593a3f498dc14b2
Bhadresh Arya <bhadresharya@hotmail.com>
2023-02-06 08:16:17 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,38 @@ import {
   Divider,
   Card,
   Button,
-  SimpleGrid
-} from '@mantine/core';
+  SimpleGrid,
+  ActionIcon,
+} from "@mantine/core";
 
 const App = () => {
   return (
     <Container p="xl">
       <Title>Mantine - React Component Library</Title>
       <Divider my="xl" />
-      <Title order={2} mb="sm">Button</Title>
-      <Card shadow="sm" radius="sm" withBorder>
-        <Title order={3} mb="sm">Variants</Title>
+      <Title order={2} mb="sm">
+        Button
+      </Title>
+      <Card shadow="sm" radius="sm" mb="md" withBorder>
+        <Title order={3} mb="sm">
+          Variants
+        </Title>
         <SimpleGrid cols={5}>
           <Button>Filled Variant</Button>
-          <Button variant='light'>Light Variant</Button>
-          <Button variant='outline'>Outline Variant</Button>
-          <Button variant='default'>Default Variant</Button>
-          <Button variant='subtle'>Subtle Variant</Button>
+          <Button variant="light">Light Variant</Button>
+          <Button variant="outline">Outline Variant</Button>
+          <Button variant="default">Default Variant</Button>
+          <Button variant="subtle">Subtle Variant</Button>
         </SimpleGrid>
       </Card>
+      <Title order={2} mb="sm">
+        Action icon
+      </Title>
+      <Card shadow="sm" radius="sm" withBorder>
+        <ActionIcon>⚙</ActionIcon>
+      </Card>
     </Container>
-  )
-}
+  );
+};
 
 export default App;
